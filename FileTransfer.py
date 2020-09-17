2a88c75b5a4125bb2aa66eaa3526e69ec9a149f0
Sam <samanthabillips@gmail.com>
2020-09-17 13:14:38 -0500

    File Transfer assignment Part one
    Your employer wants a program to move all their text files (with a .txt file extension) from one folder to another with the click of a button. On your desktop you will have two new folders, one called Folder A and another called Folder B. You will need to move text files from Folder A to Folder B.
    
    1. Create two new folders on your desktop labeled Folder A and Folder B.
    
    2. Create four .txt files, each with some meaningless content such as “this is file 1” ect., in Folder A.
    
    create a script that will transfer the files from Folder A to Folder B

---
+++ FileTransfer.py
@@ -0,0 +1,29 @@
+#
+#
+# Python Ver: 3.8.5
+#
+# Author: Samantha Billips
+#
+#
+# Assignment: File Transfer
+#   1. Create 2 new folders on desktop labeled Folder A and Folder B
+#   2. Create 4 .txt files with some meaningless content ("this is file 1",etc)
+#       in folder A
+#
+
+
+# Create a scipt that will transfer files from folder A to folder B
+
+import shutil
+import os
+
+# Set where the source of the files are
+source = '/Users/saman/Desktop/FolderA'
+
+# set destination path to FolderB
+destination = '/Users/saman/Desktop/FolderB'
+files = os.listdir(source)
+
+for i in files:
+    #move files represented by 'i' to their new destianation
+    shutil.move(source+i, destination)
