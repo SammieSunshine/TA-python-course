85763494ff53951e2f3b1980d5120689c8fd9b7f
Sam <samanthabillips@gmail.com>
2020-12-08 19:56:37 -0600

    assignment update
    created proper children for parent class; assigned additional attributes to children.

--- assignment185.py
+++ assignment185.py
@@ -10,20 +10,33 @@
 #
 
  
-class Blackboard:
-    name = 'None provided'
-    userID= ''
-    password ='blahblah'
+class Blackboard(): #Parent/base class
+    def __init__(self,name,userID, password):
+        self.name = name
+        self.userID = userID
+        self.password = password
 
-class student(Blackboard):
-    grade = 12
-    age = 18
-    GPA = 3.0
+    def printname(self):
+        print(self.name, self.userID)
 
-class teacher(Blackboard):
-    subject = ''
-    status = ''
-    awards = ''
+
+class student(Blackboard): #child class 1 
+    def __init__(self,age,grade):
+        self.age = age
+        self.grade = grade
+
+class teacher(Blackboard): #child class 2 
+    def __init__(self,subject,status,awards):
+        self.subject = subject
+        self.status = status
+        self.awards = awards
+
+
+student1 = student(13,9)
+teacher1 = teacher("Math","Active",5)
+
+print(student1.age , " " , student1.grade)
+print(teacher1.subject, " ", teacher1.awards)
 
 
     
