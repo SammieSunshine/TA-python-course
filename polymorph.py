307728055090ed854cd89f540a2b8c88184e6208
Sam <samanthabillips@gmail.com>
2020-12-11 09:49:59 -0600

    adjustments
    added python ver. info, author, assignment requirements, and included more comments

--- polymorph.py
+++ polymorph.py
@@ -1,11 +1,18 @@
-#parent class New_char
+# Python 3.8.5
+# Author: Samantha Billips
+# Polymorphism assignment 207
+# Create 2 classes that inherit from another class
+# 1. Each Child class should have at least two of their own attributes
+# 2. The parent class should have atleast one method(function)
+# 3. Both chld classes should utilize polymorphism on the parent class method
+# 4. Add comments throughout code
 
 
-
-class gameUser:
+#Parent Class
+class gameUser: 
     c_name = ""
     c_password = "424242"
-
+    
     def getcharinfo(self):
         c_nameEntry = input("Greetings, Wanderer! What is your name?:  ")
         c_passwordEntry = input("{}, eh? May I have the password?:  ".format(c_nameEntry))
@@ -13,15 +20,16 @@ class gameUser:
             print("Welcome, {}!\n\n Here's a gift that will prove helpful on your journey!\n\n *You recieve 5,000 Gold!*".format(c_nameEntry))
         
         else:
-            print("Incorrect password. You shall not pass!")
+            print("You shall not pass!")
 
-#child class for gameUser
+#child class CharInfo
 class CharInfo(gameUser):
     charRace = "Half-Elf"
     charClass = "Bard"
-    tollGold = 5000
+    tollGold = "5000"
 
-#Same method; instead of c_password, use tollGold
+#Same method seen in Parent Class
+#instead of c_password, use tollGold
     def getcharinfo(self):
         c_nameEntry = input("\n\n*You approach a bridge. You need to cross it in order to get to the city, \nbut to your dismay, it's guarded by a troll. He doesn't seem happy to see you*\n\n\"GARRGH! WHAT IS YOUR NAME?:" )
         c_tollGold = input("BAH! NO MATTER! YOU NEED TO PAY THE TROLL TOLL! HOW MUCH GOLD 'AVE YA GOT?:  ")
